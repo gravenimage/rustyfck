8e1e684a79569d65fe643736b511e5c07c8332e6
James Goldwater <james.goldwater@gmail.com>
2022-06-11 15:20:40 +0200

    Upgrade to 2018 edition and new docopt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-extern crate rustc_serialize;
-extern crate docopt;
-
 use std::fs::File;
 use std::path::Path;
 use std::io::Read;
@@ -9,18 +6,19 @@ use std::option::Option::{Some, None};
 use std::fmt::Display;
 
 use docopt::Docopt;
+use serde::Deserialize;
 
 static USAGE: &'static str = "
 Usage: rustyfck [-d -b -z -r] <source>
 
 Options:
-    -b, --brackets   
+    -b, --brackets
     -z, --zero
     -r, --rle
     -d, --debug
 ";
 
-#[derive(RustcDecodable, Debug)]
+#[derive(Deserialize, Debug)]
 struct Args {
     arg_source: String,
     flag_brackets: bool,
@@ -33,7 +31,7 @@ fn source(filename: &str) -> String {
     let mut text = String::new();
     let mut f = File::open(&Path::new(filename)).ok().expect("Cannot find input file");
     f.read_to_string(&mut text).ok().expect("Cannot read contents");
-   
+
     text
 }
 
@@ -108,7 +106,7 @@ fn run_length(op: Op, ops: &Vec<Op>, idx: usize) -> usize {
         i += 1;
         count += 1;
     }
-    count 
+    count
 }
 
 fn rle(ops: &Vec<Op>) -> Vec<Op> {
@@ -179,7 +177,7 @@ fn elide_zeroing_loop(ops: &Vec<Op>) -> Vec<Op> {
 fn match_brackets(ops: &Vec<Op>) -> Vec<Op> {
     let mut new_ops = Vec::with_capacity(ops.len());
     let mut ip: usize = 0;
-    
+
     while ip < ops.len() {
         match ops[ip] {
             Op::LoopBegin(..) => {
@@ -208,11 +206,11 @@ fn match_brackets(ops: &Vec<Op>) -> Vec<Op> {
                 }
                 new_ops.push(Op::LoopEnd(Some(begin_ip)));
             },
-	    op @ _ => { new_ops.push(op) } 
+	    op @ _ => { new_ops.push(op) }
         }
         ip += 1;
     }
-    
+
     new_ops
 }
 
@@ -220,7 +218,7 @@ fn match_brackets(ops: &Vec<Op>) -> Vec<Op> {
 fn interpret( ops: &Vec<Op>,  mem: &mut Vec<u8>, debug: DebuggingLevel) {
     let mut dp: usize = 0;
     let mut ip: usize = 0;
-    
+
     if debug > DebuggingLevel::Silent {
         println!("ops.len: {}", ops.len());
     }
@@ -237,24 +235,24 @@ fn interpret( ops: &Vec<Op>,  mem: &mut Vec<u8>, debug: DebuggingLevel) {
                 dp -= count;
                 ip += 1;
             }
-            Op::IncDerefDp(count) => { 
-                mem[dp] = mem[dp].wrapping_add(count as u8); 
-                ip += 1; 
+            Op::IncDerefDp(count) => {
+                mem[dp] = mem[dp].wrapping_add(count as u8);
+                ip += 1;
             }
-            Op::DecDerefDp(count) => { 
-                mem[dp] = mem[dp].wrapping_sub(count as u8); 
-                ip += 1; 
+            Op::DecDerefDp(count) => {
+                mem[dp] = mem[dp].wrapping_sub(count as u8);
+                ip += 1;
             }
-            Op::OutDerefDp => { 
+            Op::OutDerefDp => {
                 print!("{}", mem[dp] as char);
                 ip += 1;
             },
             Op::InDerefDp => { ip += 1 }, // no-op for now
-	    
-            Op::LoopBegin(None) => { 
+
+            Op::LoopBegin(None) => {
                 if mem[dp] != 0 {
                     ip += 1
-                } 
+                }
                 else {
                     let mut level = 1;
                     while level > 0 {
@@ -265,22 +263,22 @@ fn interpret( ops: &Vec<Op>,  mem: &mut Vec<u8>, debug: DebuggingLevel) {
                             _ => level
                         }
                     }
-                    ip +=1 ;    
+                    ip +=1 ;
                 }
             },
-	    
-            Op::LoopBegin(Some(matching)) => { 
+
+            Op::LoopBegin(Some(matching)) => {
                 if mem[dp] != 0 {
                     ip += 1;
                 }
                 else {
-                    ip = matching + 1; 
+                    ip = matching + 1;
                 }
             }
-            Op::LoopEnd(None) =>  { 
+            Op::LoopEnd(None) =>  {
                 if mem[dp] == 0 {
                     ip += 1
-                } 
+                }
                 else {
                     let mut level = 1;
                     while level > 0 {
@@ -291,13 +289,13 @@ fn interpret( ops: &Vec<Op>,  mem: &mut Vec<u8>, debug: DebuggingLevel) {
                             _ => level
                         }
                     }
-                    ip +=1 ;    
+                    ip +=1 ;
                 }
             }
             Op::LoopEnd(Some(matching)) => {
                  if mem[dp] == 0 {
                     ip += 1
-                } 
+                }
                 else {
                     ip = matching + 1;
                 }
@@ -307,7 +305,7 @@ fn interpret( ops: &Vec<Op>,  mem: &mut Vec<u8>, debug: DebuggingLevel) {
                 ip += 1;
             }
         }
-    }    
+    }
 }
 
 
@@ -315,9 +313,9 @@ fn dump_instructions(ops: &Vec<Op>) {
     let mut indent = 0;
     for op in ops.iter() {
         match *op {
-            Op::LoopBegin(_) => { 
+            Op::LoopBegin(_) => {
                 let indent_str = std::iter::repeat(" ").take(indent).collect::<String>();
-                print!("\n{}", indent_str); 
+                print!("\n{}", indent_str);
                 indent += 1;
             },
             Op::LoopEnd(_) => {
@@ -325,7 +323,7 @@ fn dump_instructions(ops: &Vec<Op>) {
             }
             _ => {}
         }
-        
+
         print!("{}", op);
     }
     println!("");
@@ -333,10 +331,10 @@ fn dump_instructions(ops: &Vec<Op>) {
 
 fn main() {
     let mem_size = 32000;
-    let args: Args = Docopt::new(USAGE)
-                            .and_then(|d| d.decode())
-                            .unwrap_or_else(|e| e.exit());
-   
+    let args = Docopt::new(USAGE).unwrap().parse().unwrap().deserialize::<Args>().unwrap();
+                            // .and_then(|d| d.argv(arg)s//d.argv(argv().into_iter()).deserialize())
+                            // .unwrap_or_else(|e| e.exit());
+
     let code = source(&args.arg_source);
 
     let mut memory = Vec::with_capacity(mem_size);
@@ -349,7 +347,7 @@ fn main() {
     if args.flag_zero {
         ops = elide_zeroing_loop(&ops);
     }
-    
+
     if args.flag_rle {
         ops = rle(&ops);
     }
@@ -364,11 +362,11 @@ fn main() {
         dump_instructions(&ops);
     }
 
-    interpret(&ops, &mut memory,  
+    interpret(&ops, &mut memory,
               if args.flag_debug  {
-                  DebuggingLevel::Verbose 
-              } 
-              else { 
+                  DebuggingLevel::Verbose
+              }
+              else {
                   DebuggingLevel::Silent
               });
 }
@@ -394,4 +392,3 @@ fn test_rle_2() {
    let optimized = rle(&vec![Op::IncDp(1)]);
    assert_eq!(optimized, [Op::IncDp(1)]);
 }
-
